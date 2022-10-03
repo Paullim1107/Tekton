ac7b51e20620e3d7108016d462fbf832bdcec3be
Paul Lim <106897517+Paullim1107@users.noreply.github.com>
2022-10-03 13:39:42 -0700

    Create SurveyAnswerAddRequest.cs

---
+++ Net/models/SurveyAnswerAddRequest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.SurveyAnswers
+{
+    public class SurveyAnswerAddRequest: SurveyAnswerAddRequestBase
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int InstanceId { get; set; }
+
+
+    }
+}
